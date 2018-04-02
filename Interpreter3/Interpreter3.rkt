c65afc97a0d8b2b7a5bf1c311f0abc1b3c759f05
dtang1 <derek.tang.27@gmail.com>
2018-04-02 16:11:30 -0400

    updated lookup function

--- Interpreter3/Interpreter3.rkt
+++ Interpreter3/Interpreter3.rkt
@@ -28,7 +28,7 @@
 (define populate-global-variables
   (lambda (statement-list environment return break continue throw next)
     (cond
-      ((null? statement-list) (next statement-list))
+      ((null? statement-list) environment)
       ((eq? (caar statement-list) 'var) (populate-global-variables (cdr statement-list) (addGlobalVar (car statement-list) environment) return break continue throw next))
       ((eq? (caar statement-list) 'function) (populate-global-variables (cdr statement-list) (addGlobalFunc (car statement-list) environment) return break continue throw next))
       (else (myerror "error: poor global variable syntax:" (caar statement-list))))))
@@ -287,7 +287,7 @@
     (cons (remainingLocal environment) (global environment))))
 
 ; some abstractions
-(define topframe car)
+(define topframe caar)
 (define remainingframes cdr)
 
 ; does a variable exist in the environment?
